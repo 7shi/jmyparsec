4f5c435699857afadbd12b4906b010633ce9b7be
7shi <7shi@live.jp>
2016-05-11 16:08:39 +0900

    1文字取得

--- src/JParsec.java
+++ src/JParsec.java
@@ -1,6 +1,11 @@
 
 public class JParsec {
 
+    public static char anyChar(String s) {
+        return s.charAt(0);
+    }
+
     public static void main(String[] args) {
+        System.out.println(anyChar("abc"));
     }
 }
