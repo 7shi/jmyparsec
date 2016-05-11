71bf2f071d3947927ecd0f90f21309f133031f6b
7shi <7shi@live.jp>
2016-05-11 16:53:33 +0900

    メソッド化

--- src/JParsec.java
+++ src/JParsec.java
@@ -7,9 +7,15 @@ public class JParsec {
         return ret;
     }
 
+    public static String test1(Source s) {  // メソッド化
+        char x1 = anyChar(s);
+        char x2 = anyChar(s);
+        return new String(new char[]{x1, x2});
+    }
+
     public static void main(String[] args) {
         Source s = new Source("abc");
         System.out.println(anyChar(s));
-        System.out.println(anyChar(s));
+        System.out.println(test1(s));
     }
 }
