b16b8e9929ec8ed37eb075b1238702db473f5069
7shi <7shi@live.jp>
2016-05-11 16:57:12 +0900

    組み合わせ

--- src/JParsec.java
+++ src/JParsec.java
@@ -7,15 +7,24 @@ public class JParsec {
         return ret;
     }
 
-    public static String test1(Source s) {  // メソッド化
+    public static String test1(Source s) {
         char x1 = anyChar(s);
         char x2 = anyChar(s);
         return new String(new char[]{x1, x2});
     }
 
+    public static String test2(Source s) {  // 追加
+        String x1 = test1(s);
+        char x2 = anyChar(s);
+        return x1 + x2;
+    }
+
     public static void main(String[] args) {
-        Source s = new Source("abc");
-        System.out.println(anyChar(s));
-        System.out.println(test1(s));
+        Source s1 = new Source("abc");
+        System.out.println(anyChar(s1));
+        Source s2 = new Source("abc");
+        System.out.println(test1(s2));
+        Source s3 = new Source("abc");
+        System.out.println(test2(s3));
     }
 }
