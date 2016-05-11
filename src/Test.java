04d28e97f7fc8dd25645c3b07459bd9e5e9b0472
7shi <7shi@live.jp>
2016-05-11 19:11:33 +0900

    組み合わせ判定

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,20 @@
 
+import JParsec.*;
 import static JParsec.Parsers.*;
 
 public class Test {
 
+    public static final Parser<String> test3 = s -> {
+        char x1 = letter.parse(s);
+        char x2 = digit.parse(s);
+        char x3 = digit.parse(s);
+        return new String(new char[]{x1, x2, x3});
+    };
+
     public static void main(String[] args) {
-        parseTest(digit, "abc");  // NG
-        parseTest(digit, "123");
-        parseTest(letter, "abc");
-        parseTest(letter, "123");  // NG
+        parseTest(test3, "abc");  // NG
+        parseTest(test3, "123");  // NG
+        parseTest(test3, "a23");
+        parseTest(test3, "a234");
     }
 }
