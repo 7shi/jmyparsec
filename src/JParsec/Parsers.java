2fa6a45b3732b539977ca838c206d9b7dcee76e9
7shi <7shi@live.jp>
2016-05-11 19:07:45 +0900

    事前定義

--- src/JParsec/Parsers.java
+++ src/JParsec/Parsers.java
@@ -29,4 +29,15 @@ public class Parsers {
     public static final Parser<Character> char1(char ch) {
         return satisfy(c -> c == ch);
     }
+
+    public static final boolean isAlphaNum(char ch) {
+        return Character.isAlphabetic(ch) || Character.isDigit(ch);
+    }
+
+    public static final Parser<Character> digit = satisfy(Character::isDigit);
+    public static final Parser<Character> upper = satisfy(Character::isUpperCase);
+    public static final Parser<Character> lower = satisfy(Character::isLowerCase);
+    public static final Parser<Character> alpha = satisfy(Character::isAlphabetic);
+    public static final Parser<Character> alphaNum = satisfy(Parsers::isAlphaNum);
+    public static final Parser<Character> letter = satisfy(Character::isLetter);
 }
--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,9 @@ import static JParsec.Parsers.*;
 public class Test {
 
     public static void main(String[] args) {
-        parseTest(char1('a'), "abc");
-        parseTest(char1('a'), "123");  // NG
+        parseTest(digit, "abc");  // NG
+        parseTest(digit, "123");
+        parseTest(letter, "abc");
+        parseTest(letter, "123");  // NG
     }
 }
