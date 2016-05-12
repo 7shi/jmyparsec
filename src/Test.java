257ff2e9b4ecf446e57c6f4429b30395abfc1ea1
7shi <7shi@live.jp>
2016-05-12 11:57:28 +0900

    共通部分（修正）

--- src/Test.java
+++ src/Test.java
@@ -9,9 +9,12 @@ public class Test {
     static final Parser<Character> c = char1('c');
 
     static final Parser<String> test8 = or(sequence(a, b), sequence(a, c));
+    static final Parser<String> test9 = sequence(a, or(b, c));
 
     public static void main(String[] args) {
         parseTest(test8, "ab");
         parseTest(test8, "ac");  // NG
+        parseTest(test9, "ab");
+        parseTest(test9, "ac");
     }
 }
