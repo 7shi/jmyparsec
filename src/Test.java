3334afa4edefd389ed1c704e41001d5a5e074465
7shi <7shi@live.jp>
2016-05-12 19:42:02 +0900

    整数化

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,21 @@
 
 import java.util.List;
+import java.util.function.Function;  // 追加
 import jmyparsec.*;
 import static jmyparsec.Parsers.*;
 
 public class Test {
 
-    static final Parser<Integer> number = s -> {
-        return Integer.parseInt(many1(digit).parseToString(s));
-    };
+    static final Function<List<Character>, Integer> toInt              // 追加
+            = s -> Integer.parseInt(Parser.toString(s));
+
+    static final Parser<Integer> number = apply(toInt, many1(digit));  // 適用
 
     static final Parser<Integer> expr = s -> {
         int x = number.parse(s);
         List<Integer> xs = many(or(
                 char1('+').next(    number ),
-                char1('-').next(neg(number))  // 適用
+                char1('-').next(neg(number))
         )).parse(s);
         return xs.stream().reduce(x, Integer::sum);
     };
@@ -23,7 +25,7 @@ public class Test {
         parseTest(expr  , "1+2"  );
         parseTest(expr  , "123"  );
         parseTest(expr  , "1+2+3");
-        parseTest(expr  , "1-2-3");  // OK
-        parseTest(expr  , "1-2+3");  // OK
+        parseTest(expr  , "1-2-3");
+        parseTest(expr  , "1-2+3");
     }
 }
