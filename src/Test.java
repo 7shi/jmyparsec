35ef06910e9a346e9c399053907622d4e54d055f
7shi <7shi@live.jp>
2016-05-12 19:07:28 +0900

    コードの整理

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,25 @@
 
+import java.util.List;
+import jmyparsec.*;
 import static jmyparsec.Parsers.*;
 
 public class Test {
 
+    static final Parser<Integer> number = s -> {
+        return Integer.parseInt(many1(digit).parseToString(s));
+    };
+
+    static final Parser<List<Integer>> expr = s -> {
+        int x = number.parse(s);
+        List<Integer> xs = many(char1('+').next(number)).parse(s);  // 簡略化
+        xs.add(0, x);
+        return xs;
+    };
+
     public static void main(String[] args) {
-        parseTest(sequence(letter, digit), "a1");
-        parseTest(letter.prev(digit), "a1");
-        parseTest(letter.next(digit), "a1");
+        parseTest(number, "123"  );
+        parseTest(expr  , "1+2"  );
+        parseTest(expr  , "123"  );
+        parseTest(expr  , "1+2+3");
     }
 }
