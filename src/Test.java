20f6267dcabdcdf4fbded9234e7f28d94765227d
7shi <7shi@live.jp>
2016-05-12 11:01:29 +0900

    最初に出て来たサンプル

--- src/Test.java
+++ src/Test.java
@@ -4,10 +4,15 @@ import static jparsec.Parsers.*;
 
 public class Test {
 
-    static final Parser<String> test4 = many(alpha);
+    public static void main(String[] args) throws Exception {
+        Source s1 = new Source("abc123");
+        String s1a = many(alpha).parse(s1);
+        String s1b = many(digit).parse(s1);
+        System.out.println(s1a + "," + s1b);
 
-    public static void main(String[] args) {
-        parseTest(test4, "abc123");
-        parseTest(test4, "123abc");
+        Source s2 = new Source("abcde9");
+        String s2a = many(alpha).parse(s2);
+        String s2b = many(digit).parse(s2);
+        System.out.println(s2a + "," + s2b);
     }
 }
