012e5509de91a2a704d8bac9e260d61fe00fed87
7shi <7shi@live.jp>
2016-05-12 19:15:24 +0900

    計算

--- src/Test.java
+++ src/Test.java
@@ -9,17 +9,16 @@ public class Test {
         return Integer.parseInt(many1(digit).parseToString(s));
     };
 
-    static final Parser<List<Integer>> expr = s -> {
+    static final Parser<Integer> expr = s -> {  // 戻り値の型 -> Integer
         int x = number.parse(s);
-        List<Integer> xs = many(char1('+').next(number)).parse(s);  // 簡略化
-        xs.add(0, x);
-        return xs;
+        List<Integer> xs = many(char1('+').next(number)).parse(s);
+        return xs.stream().reduce(x, Integer::sum);  // 合計
     };
 
     public static void main(String[] args) {
         parseTest(number, "123"  );
-        parseTest(expr  , "1+2"  );
-        parseTest(expr  , "123"  );
-        parseTest(expr  , "1+2+3");
+        parseTest(expr  , "1+2"  );  // OK
+        parseTest(expr  , "123"  );  // OK
+        parseTest(expr  , "1+2+3");  // OK
     }
 }
