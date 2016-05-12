5cb99b7767960f4e48d1dc2f0c334e968869d76d
7shi <7shi@live.jp>
2016-05-12 16:31:09 +0900

    数値

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,9 @@ import static jmyparsec.Parsers.*;
 
 public class Test {
 
-    static final Parser<String> number = many1(digit);
+    static final Parser<Integer> number = s -> {  // 修正
+        return Integer.parseInt(many1(digit).parse(s));
+    };
 
     public static void main(String[] args) {
         parseTest(number, "123");
