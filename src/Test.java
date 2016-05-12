ede211f0af380aaabe9158b7949f8927d1c71a1c
7shi <7shi@live.jp>
2016-05-12 11:26:09 +0900

    結合と選択

--- src/Test.java
+++ src/Test.java
@@ -4,10 +4,15 @@ import static jparsec.Parsers.*;
 
 public class Test {
 
-    static final Parser<String> test6 = many(or(letter, digit));
+    static final Parser<Character> a = char1('a');
+    static final Parser<Character> b = char1('b');
+    static final Parser<Character> c = char1('c');
+
+    static final Parser<String> test7 = or(sequence(a, b), sequence(c, b));
 
     public static void main(String[] args) {
-        parseTest(test6, "abc123");
-        parseTest(test6, "123abc");
+        parseTest(test7, "ab");
+        parseTest(test7, "cb");
+        parseTest(test7, "acb");  // ???
     }
 }
