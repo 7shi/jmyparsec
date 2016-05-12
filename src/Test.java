f668dc38c08dce30d2254679d18b55ed28e24523
7shi <7shi@live.jp>
2016-05-12 11:50:35 +0900

    まとめの再掲

--- src/Test.java
+++ src/Test.java
@@ -4,15 +4,38 @@ import static jmyparsec.Parsers.*;
 
 public class Test {
 
-    static final Parser<Character> a = char1('a');
-    static final Parser<Character> b = char1('b');
-    static final Parser<Character> c = char1('c');
+    static final Parser<String> test1 = sequence(anyChar, anyChar);
+    static final Parser<String> test2 = sequence(test1, anyChar);
+    static final Parser<String> test3 = sequence(letter, replicate(2, digit));
+    static final Parser<String> test4 = many(alpha);
 
-    static final Parser<String> test7 = or(sequence(a, b), sequence(c, b));
+    public static void main(String[] args) throws Exception {
+        parseTest(anyChar   , "abc"   );
+        parseTest(test1     , "abc"   );
+        parseTest(test2     , "abc"   );
+        parseTest(test2     , "12"    );  // NG
+        parseTest(test2     , "123"   );
+        parseTest(char1('a'), "abc"   );
+        parseTest(char1('a'), "123"   );  // NG
+        parseTest(digit     , "abc"   );  // NG
+        parseTest(digit     , "123"   );
+        parseTest(letter    , "abc"   );
+        parseTest(letter    , "123"   );  // NG
+        parseTest(test3     , "abc"   );  // NG
+        parseTest(test3     , "123"   );  // NG
+        parseTest(test3     , "a23"   );
+        parseTest(test3     , "a234"  );
+        parseTest(test4     , "abc123");
+        parseTest(test4     , "123abc");
 
-    public static void main(String[] args) {
-        parseTest(test7, "ab");
-        parseTest(test7, "cb");
-        parseTest(test7, "acb");  // ???
+        Source s1 = new Source("abc123");
+        String s1a = many(alpha).parse(s1);
+        String s1b = many(digit).parse(s1);
+        System.out.println(s1a + "," + s1b);
+
+        Source s2 = new Source("abcde9");
+        String s2a = many(alpha).parse(s2);
+        String s2b = many(digit).parse(s2);
+        System.out.println(s2a + "," + s2b);
     }
 }
